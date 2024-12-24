f5499985323a073fe6561c29f88ce0055091755d
masoud.varziii <masoud.varziii@gmail.com>
2024-12-24 16:25:32 +0330

    feat: added routes container to main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
- 
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
+
 const App = () => {
   return (
-    <div className="text-cyan-50">
-      hellp
-    </div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/signup" element={Signup} />
+        <Route path="/login" element={Login} />
+        <Route path="/settings" element={Settings} />
+        <Route path="/profile" element={Profile} />
+      </Routes>
+      <Navbar />
+    </>
+  );
+};
 
-export default App
+export default App;
