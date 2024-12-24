5ca2965f3d4e330d2cfdc09c5a2bacf6e42ccb93
masoud.varziii <masoud.varziii@gmail.com>
2024-12-24 14:52:59 +0330

    fix: moved message controller to controller folder

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,7 @@
 const Message = require("../models/message.model");
 const User = require("../models/user.model");
+const cloudinary = require("../lib/cloudinary");
+
 
 const getAllUsers = async (req, res) => {
   try {
@@ -37,4 +39,32 @@ const getMessages = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers , getMessages };
+const sendMessage = async ()=> {
+  try {
+    const { id: recieverId } = req.params;
+    const senderId = req.user._id;
+    const {text , image} = req.body;
+    let imageUrl;
+    if(image) {
+      const uploadedResponse = await cloudinary.uploader.upload(image)
+      imageUrl = uploadedResponse.secure_url
+    }
+    const newMessage = new Message({
+      text,
+      recieverId,
+      senderId,
+      image: imageUrl
+    })
+
+    await newMessage.save()
+    return res.status(201).json(newMessage)
+
+  } catch (error) {
+    console.log("Error in sendMessage controller", error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+module.exports = { getAllUsers , getMessages , sendMessage };
