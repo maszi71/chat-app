faad64b6ece92e091c7a7ef4db649d720ac39b60
masoud.varziii <masoud.varziii@gmail.com>
2024-12-30 17:19:47 +0330

    feat: implemented showing profile information

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,90 @@
+import { Camera, Mail, User } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Profile = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { authUser, updateProfile, isUpdatingProfile } = useAuthStore();
 
-export default Profile
+  const handleImageUpload = (e) => {};
+
+  return (
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold">Profile</h1>
+            <p className="mt-2">Your Profile Information</p>
+          </div>
+          {/* upload avatar section */}
+          <div className="flex flex-col items-center gap-4">
+            <div className="relative">
+              <img
+                src={authUser.profilePic || "/avatar.png"}
+                alt="profile"
+                className="size-32 rounded-full object-cover border-4"
+              />
+              <label
+                className={`absolute bottom-0 right-0 bg-base-content hover:scale-105 p-2 rounded-full cursor-pointer transition-all duration-200 ${
+                  isUpdatingProfile ? "animate-pulse pointer-events-none" : ""
+                }`}
+                htmlFor="avatar-upload"
+              >
+                <Camera className="w-5 h-5 text-base-200" />
+                <input
+                  type="file"
+                  id="avatar-upload"
+                  className="hidden"
+                  accept="image/*"
+                  disabled={isUpdatingProfile}
+                  onChange={handleImageUpload}
+                />
+              </label>
+            </div>
+            <p className="text-sm text-zinc-400">
+              {isUpdatingProfile
+                ? "Uploading"
+                : "click the camera icon to upload your photo"}
+            </p>
+          </div>
+
+          <div className="space-y-6">
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <User className="w-4 h-4" />
+                Full Name
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.fullName}
+              </p>
+            </div>
+
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <Mail className="w-4 h-4" />
+                Email Address
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.email}
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-6 bg-base-300 rounded-xl p-6">
+            <h2 className="text-lg font-medium mb-4">Account Information</h2>
+            <div className="space-y-3 text-sm">
+              <div className="flex items-center justify-between py-2 border-b border-zinc-700">
+                <span>Member Since</span>
+                <span>{authUser?.createdAt.split("T")[0]}</span>
+              </div>
+              <div className="flex items-center justify-between py-2">
+                <span>Account Status</span>
+                <span className="text-green-500">Active</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
