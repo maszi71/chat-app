4e356d93c77bda2f6a102f8b4ed83c0068fe34e4
masoudvrz <masoud.varzi@yahoo.com>
2025-01-01 15:56:45 +0330

    feat: implemeted different themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,11 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { checkAuth, authUser, isCheckingAuth } = useAuthStore();
+  const {theme} = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -25,7 +27,7 @@ const App = () => {
     );
   }
   return (
-    <>
+    <div data-theme={theme}>
       <Routes>
         <Route
           path="/"
@@ -47,7 +49,7 @@ const App = () => {
       </Routes>
       <Navbar />
       <Toaster  />
-    </>
+    </div>
   );
 };
 
